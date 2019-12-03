992e3847998ab0828e109440bec60e958388714d
Kristoffer Spencer <whobanil80@linuxmail.org>
2019-12-03 07:50:03 -0500

    Login page is waiting on server and next component

--- PokeManiaWebApp/src/app/login/components/login/login.component.ts
+++ PokeManiaWebApp/src/app/login/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../../models/User'
+import { HttpClient } from '@angular/common/http'
+import { LoginForm } from 'src/app/models/LoginForm';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public username: string = ''
+  public password: string = ''
+  public errorExists: boolean = false
+  public errorMsg: string = ''
+
+  constructor(private userService: UserService, private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  login() {
+
+    let user: User
+
+    if(!this.checkErrors()) return
+
+    user = this.getUser()
+
+    if(user !== null) {
+    
+      this.userService.setUser(user);
+      //Route to next component
+
+    } else this.displayError('Invalid login')
+
+  }
+
+  getUser(): User {
+
+    let loginInfo: LoginForm = new LoginForm(this.username, this.password)
+    const response = this.http.post('url', JSON.stringify(loginInfo))
+
+    //Get data from response and put into a User obj and return
+    return null
+
+  }
+
+  checkErrors() {
+
+    if(this.username === '') {
+
+      this.displayError('Username must be provided!')
+      return false
+
+    }
+
+    if(this.password === '') {
+
+      this.displayError('Password must be provided!')
+      return false
+
+    }
+
+    return true
+
+  }
+
+  displayError(msg: string) {
+
+    this.errorExists = true
+    this.errorMsg = msg
+
+    setTimeout(() => {
+
+      this.errorExists = false
+      this.errorMsg = ''
+
+    }, 3000);
+
   }
 
 }
--- PokeManiaWebApp/src/app/login/login.module.ts
+++ PokeManiaWebApp/src/app/login/login.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { UserService } from './services/user.service';
+import { FormsModule } from '@angular/forms'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 
 
 @NgModule({
+  providers: [UserService, HttpClient],
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class LoginModule { }
