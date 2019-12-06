b7f1c7c8224aa100cdb52de01a747556232e8c52
Kristoffer Spencer <whobanil80@linuxmail.org>
2019-12-06 00:58:14 -0500

    Added the registeration page. It's got nice feedback on error

--- PokeManiaWebApp/src/app/app-routing.module.ts
+++ PokeManiaWebApp/src/app/app-routing.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { LoginComponent } from './login/components/login/login.component';
 import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module'
+import { RegisterComponent } from './register/components/register/register.component'
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ]
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    LoginModule
+    LoginModule,
+    RegisterModule
   ],
   exports: [
     RouterModule
